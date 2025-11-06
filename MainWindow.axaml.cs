71cb633a2b5a45f16b8b355cc3a8132f42812434
Aribzhan <aribzankamilzanov@gmail.com>
2025-11-06 14:24:39 +0500

    new fix

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,26 +1,26 @@
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using MsBox.Avalonia;
-using MsBox.Avalonia.Enums;
+// using Avalonia.Controls;
+// using Avalonia.Interactivity;
+// using MsBox.Avalonia;
+// using MsBox.Avalonia.Enums;
 
-namespace HelloAvalonia;
+// namespace HelloAvalonia;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+// public partial class MainWindow : Window
+// {
+//     public MainWindow()
+//     {
+//         InitializeComponent();
+//     }
 
-    private async void ButtonHello_Click(object? sender, RoutedEventArgs e)
-    {
-        var box = MessageBoxManager.GetMessageBoxStandard(
-            "Приветствие",
-            "Привет, меня зовут Aribzhan!",
-            ButtonEnum.Ok);
-        await box.ShowAsync();
-    }
-}
+//     private async void ButtonHello_Click(object? sender, RoutedEventArgs e)
+//     {
+//         var box = MessageBoxManager.GetMessageBoxStandard(
+//             "Приветствие",
+//             "Привет, меня зовут Aribzhan!",
+//             ButtonEnum.Ok);
+//         await box.ShowAsync();
+//     }
+// }
 
 
 // using Avalonia.Controls;
@@ -79,3 +79,76 @@ public partial class MainWindow : Window
 //         }
 //     }
 // }
+
+
+
+
+
+
+// using Avalonia.Controls;
+// using Avalonia.Input;
+// using Avalonia.Media;
+// using System;
+
+// namespace HelloAvalonia
+// {
+//     public partial class MainWindow : Window
+//     {
+//         private Random random = new Random();
+
+//         public MainWindow()
+//         {
+//             InitializeComponent();
+//         }
+
+//         private void ColorButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+//         {
+//             // Случайный цвет
+//             var color = Color.FromRgb(
+//                 (byte)random.Next(0, 256),
+//                 (byte)random.Next(0, 256),
+//                 (byte)random.Next(0, 256));
+
+//             this.Background = new SolidColorBrush(color);
+//         }
+
+//         private void TitleTextBox_TextChanged(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+//         {
+//             this.Title = TitleTextBox.Text;
+//         }
+
+//         private void Window_PointerMoved(object? sender, PointerEventArgs e)
+//         {
+//             var point = e.GetPosition(this);
+//             CoordsText.Text = $"Координаты: X={point.X:F0}, Y={point.Y:F0}";
+//         }
+//     }
+// }
+
+
+
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace HelloAvalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GreenButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Green);
+            this.Title = "Цвет изменён на зелёный";
+        }
+
+        private void BlueButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Blue);
+            this.Title = "Цвет изменён на синий";
+        }
+    }
+}
